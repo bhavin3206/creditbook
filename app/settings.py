566f37d07f26f59701efea69235bfbd3ea106230
bhavin3206 <bhavinpatel99987@gmail.com>
2025-05-16 16:25:11 +0530

    `Updated EMAIL_HOST_USER and EMAIL_HOST_PASSWORD in settings.py`

--- app/settings.py
+++ app/settings.py
@@ -208,8 +208,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER="bhavinpatel99987@gmail.com"
+EMAIL_HOST_PASSWORD="tquw dzdf vpfq kjif"
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
